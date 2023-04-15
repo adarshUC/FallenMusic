079c0818bfedfcbc85298e5145f3101c09e09df9
adarshUC <121399431+adarshUC@users.noreply.github.com>
2023-04-15 19:00:40 +0530

    Update __init__.py
    Signed-off-by: adarshUC <121399431+adarshUC@users.noreply.github.com>

--- FallenMusic/__init__.py
+++ FallenMusic/__init__.py
@@ -92,8 +92,8 @@ async def fallen_startup():
     ASS_USERNAME = getme2.username
     ASS_MENTION = getme2.mention
     try:
-        await app2.join_chat("DevilsHeavenMF")
-        await app2.join_chat("FallenAssociation")
+        await app2.join_chat("scenario_support")
+        await app2.join_chat("scenario_updates")
     except:
         pass
 
